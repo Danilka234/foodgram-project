cd09323256e1f531a3704944edcfba51945e00b8
DanilLTC <yakushev.danil@gmail.com>
2023-11-02 20:24:53 +0300

    Deploy 2.1

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -15,15 +15,15 @@ def download_cart(recipes):
             measurement_unit=F("ingredient__measurement_unit")
         ).annotate(amount=Sum("amount"))
         shop_list += "\n".join([
-            f'* {ingredient["name"]} '
-            f'({ingredient["measurement_unit"]})'
-            f' - {ingredient["amount"]}'
+            f"* {ingredient['name']} "
+            f"({ingredient['measurement_unit']})"
+            f" - {ingredient['amount']}"
             for ingredient in ingredients
 
         ])
         shop_list += "\n\n"
     today = datetime.today()
     file = f"Список покупок от {today:%Y-%m-%d}.txt"
-    response = HttpResponse(shop_list, content_type='text/plain')
+    response = HttpResponse(shop_list, content_type="text/plain")
     response["Content-Disposition"] = f"attachment; filename={file}"
     return response
